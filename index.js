e3c9639f1cc7212d59e23d53f7211991f13a433d
Amrish Fernan Rajeev <amrish.r@wphdigital.com>
2026-03-27 10:16:21 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,116 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+let passwordOne = document.getElementById("pass1");
+let passwordTwo = document.getElementById("pass2");
+let pass1 = "";
+let pass2 = "";
+
+function generatePassword() {
+  passwordOne = "";
+  passwordOne = document.getElementById("pass1");
+  pass1 = "";
+  pass2 = "";
+
+  for (let i = 0; i < 15; i++) {
+    let randPass1 = Math.floor(Math.random() * characters.length);
+    let randPass2 = Math.floor(Math.random() * characters.length);
+
+    console.log(randPass1);
+    pass1 += characters[randPass1];
+    pass2 += characters[randPass2];
+
+    console.log(pass1);
+  }
+  return [(passwordOne.textContent = pass1), (passwordTwo.textContent = pass2)];
+}
